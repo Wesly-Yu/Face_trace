c40b0322bd685fdc029b80ef536516318e1c7657
Administrator <1633235633@qq.com>
2019-06-21 13:55:57 +0800

    生成验证码--修改模型的训练次数

--- trace_manager/create_number.py
+++ trace_manager/create_number.py
@@ -88,6 +88,7 @@ def vec2text(vec):
 
 # 定义cnn
 def crack_captcha_cnn(w_alpha=0.01,b_alpha=0.1):
+	model = tf.keras.Sequential()
 	x = tf.reshape(X,shape=[-1,Image_height,Image_width,1])   #-1表示让tensorflow  自动计算batch的值,最后的1表示图像的通道数，由于已经转为灰度图像，所以通道数为1
 	#3层卷积神经网络--01
 	w_c1 =tf.Variable(w_alpha*tf.random_normal([3,3,1,32]))
@@ -120,7 +121,7 @@ def crack_captcha_cnn(w_alpha=0.01,b_alpha=0.1):
 	out = tf.add(tf.matmul(dense,w_out),b_out)
 	return out
 #训练
-def train_crack_captcha_cnn():
+def train_captcha_cnn():
 	output = crack_captcha_cnn()
 	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(output, Y))  #定义损失函数,计算sigmoid的交叉熵,衡量的是分类任务中的概率误差,reduce_mean:降维或者计算tensor（图像）的平均值
 	optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)    #自适应矩阵优化,定义优化器
@@ -153,25 +154,27 @@ def train_crack_captcha_cnn():
 
 
 
+def crack_captcha_cnn():
+    model = tf.keras.Sequential()
+	#第一层神经卷积网络
+    model.add(tf.keras.layers.Conv2D(32, (3, 3)))
+    model.add(tf.keras.layers.PReLU())
+    model.add(tf.keras.layers.MaxPool2D((2, 2), strides=2))
+	#第二层神经卷积网络
+    model.add(tf.keras.layers.Conv2D(64, (5, 5)))
+    model.add(tf.keras.layers.PReLU())
+    model.add(tf.keras.layers.MaxPool2D((2, 2), strides=2))
+	#第三层神经卷积网络
+    model.add(tf.keras.layers.Conv2D(128, (5, 5)))
+    model.add(tf.keras.layers.PReLU())
+    model.add(tf.keras.layers.MaxPool2D((2, 2), strides=2))
+	#连接层优化
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(MAX_CAPTCHA * CHAR_SET_LEN))
+    model.add(tf.keras.layers.Reshape([MAX_CAPTCHA, CHAR_SET_LEN]))
+    model.add(tf.keras.layers.Softmax())
 
-# # 生成一个训练batch ：生成训练样本
-# def get_next_batch(batch_size=128):
-#     batch_x = np.zeros([batch_size, IMAGE_HEIGHT*IMAGE_WIDTH])
-#     batch_y = np.zeros([batch_size, MAX_CAPTCHA*CHAR_SET_LEN])
-# 	#判断生成的验证码图片是否符合我们的要求（60,160,3） 3表示3通道
-# 	def wrap_gen_text_image():
-# 		while True:
-# 			text, image = gen_captcha_text_image()
-# 			if image.shape == (60, 160, 3):
-# 				return text, image
-#
-# 	for i in range(batch_size):
-# 		text, image = wrap_gen_text_image()
-# 		image = convert_gray(image)
-# 		batch_x[i,:]=image.flatten()/255
-# 		batch_y[1,:]=text2vec(text)
-# 	return batch_x,batch_y
-
+    return model
 
 
 
@@ -185,11 +188,11 @@ def train_robot():
 	keep_prob = tf.placeholder(tf.float32)
 
 
-def get_next_batch(batch_size=64):
+def get_next_batch(batch_size=128):
 	batch_x = np.zeros([batch_size, IMAGE_HEIGHT * IMAGE_WIDTH])
 	batch_y = np.zeros([batch_size, MAX_CAPTCHA * CHAR_SET_LEN])
 
-	# 有时生成图像大小不是(60, 160, 3)
+	#判断图像大小不是(60, 160, 3)  3表示图像的通道数
 	def wrap_gen_captcha_text_and_image():
 		while True:
 			text, image = gen_captcha_text_and_image()
@@ -221,7 +224,7 @@ if __name__ == '__main__':
 		X = tf.placeholder(tf.float32,[None, height*width])  # 计算有多少个像素点
 		Y = tf.placeholder(tf.float32,[None, max_captcha*char_set_len])  # 计算有多少位数字   4x10
 		keep_prob = tf.placeholder(tf.float32)
-		train_crack_captcha_cnn()
+		train_captcha_cnn()
 	if train ==1:
 		number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 		IMAGE_HEIGHT = 60
