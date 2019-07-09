02932fa6b47da79867d27a739f10e6c1509fcf17
Wesly-Yu <1633235633@qq.com>
2019-07-09 20:21:19 +0800

    修改运行时一些报错

--- trace_manager/create_number.py
+++ trace_manager/create_number.py
@@ -1,13 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import  cm
-from PIL import Image,ImageDraw,ImageFont,ImageFilter
+from PIL import Image
 import random
 from captcha.image import ImageCaptcha
-import math,string
-import os
-import io
+
 
 number = ['0','1','2','3','4','5','6','7','8','9']
 # alpha=['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -30,7 +27,7 @@ width, height = size
 
 
 #创建4位验证码数字
-def radom_captcha_text(char_set=number,captsize=4):
+def radom_captcha_text(char_set=number, captsize=4):
 	captcha_text = []
 	for i in range(captsize):
 		char = random.choice(char_set)
@@ -89,7 +86,7 @@ def vec2text(vec):
 # 定义cnn
 def defin_cnn(w_alpha=0.01,b_alpha=0.1):
 	model = tf.keras.Sequential()
-	x = tf.reshape(X,shape=[-1,Image_height,Image_width,1])   #-1表示让tensorflow  自动计算batch的值,最后的1表示图像的通道数，由于已经转为灰度图像，所以通道数为1
+	x = tf.reshape(X,shape=[-1,IMAGE_HEIGHT,IMAGE_WIDTH,1])   #-1表示让tensorflow  自动计算batch的值,最后的1表示图像的通道数，由于已经转为灰度图像，所以通道数为1
 	#3层卷积神经网络--01
 	w_c1 =tf.Variable(w_alpha*tf.random_normal([3,3,1,32]))
 	b_c1 =tf.Variable(b_alpha*tf.random_normal([32]))
@@ -116,14 +113,14 @@ def defin_cnn(w_alpha=0.01,b_alpha=0.1):
 	dense = tf.nn.relu(tf.add(tf.matmul(dense,w_d),b_d))
 	dense = tf.nn.dropout(dense,keep_prob)
 	# 全连接层--002
-	w_out = tf.Variable(w_alpha*tf.random_normal([1024,max_captcha*char_set_len]))
-	b_out = tf.Variable(b_alpha*tf.random_normal([max_captcha*char_set_len]))
+	w_out = tf.Variable(w_alpha*tf.random_normal([1024,MAX_CAPTCHA*CHAR_SET_LEN]))
+	b_out = tf.Variable(b_alpha*tf.random_normal([MAX_CAPTCHA*CHAR_SET_LEN]))
 	out = tf.add(tf.matmul(dense,w_out),b_out)
 	return out
 #训练
 def train_cnn():
 	output = defin_cnn()
-	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(output, Y))  #定义损失函数,计算sigmoid的交叉熵,衡量的是分类任务中的概率误差,reduce_mean:降维或者计算tensor（图像）的平均值
+	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=output,logits=Y))  #定义损失函数,计算sigmoid的交叉熵,衡量的是分类任务中的概率误差,reduce_mean:降维或者计算tensor（图像）的平均值
 	optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)    #自适应矩阵优化,定义优化器
 	predict = tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN])    #定义预测函数
 	max_idx_p = tf.argmax(predict, 2)  #tf.argmax返回每行或者每列最大值的索引
@@ -166,13 +163,13 @@ def train_cnn():
 
 
 
-def train_robot():
-	max_captcha = len(text)  # 验证码长度
-	char_set = number + alpha + Alpha
-	char_set_len = len(char_set)
-	X= tf.placeholder(tf.float32,[None,height*width])   #计算有多少个像素点
-	Y = tf.placeholder(tf.float32,[None,max_captcha*char_set_len])  #计算有多少位数字
-	keep_prob = tf.placeholder(tf.float32)
+# def train_robot():
+# 	max_captcha = len(text)  # 验证码长度
+# 	char_set = number + alpha + Alpha
+# 	char_set_len = len(char_set)
+# 	X= tf.placeholder(tf.float32,[None,height*width])   #计算有多少个像素点
+# 	Y = tf.placeholder(tf.float32,[None,max_captcha*char_set_len])  #计算有多少位数字
+# 	keep_prob = tf.placeholder(tf.float32)
 
 
 def get_train_batch(batch_size=128):
@@ -182,19 +179,31 @@ def get_train_batch(batch_size=128):
 	#判断图像大小不是(60, 160, 3)  3表示图像的通道数
 	def wrap_gen_captcha_text_and_image():
 		while True:
-			text, image = gen_captcha_text_and_image()
+			text, image = gen_captcha_text_image()
 			if image.shape == (60, 160, 3):
 				return text, image
 
 	for i in range(batch_size):
 		text, image = wrap_gen_captcha_text_and_image()
-		image = convert2gray(image)
+		image = convert_gray(image)
 
 		batch_x[i, :] = image.flatten() / 255  # (image.flatten()-128)/128  mean为0
 		batch_y[i, :] = text2vec(text)
 
 	return batch_x, batch_y
 
+
+def crack_captcha(captcha_image):
+	output = defin_cnn()
+
+	saver = tf.train.Saver()
+	with tf.Session() as sess:
+		saver.restore(sess, "./model/crack_capcha.model-810")
+
+		predict = tf.argmax(tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
+		text_list = sess.run(predict, feed_dict={X: [captcha_image], keep_prob: 1})
+		text = text_list[0].tolist()
+		return text
 
 if __name__ == '__main__':
 	train = 0
@@ -203,13 +212,13 @@ if __name__ == '__main__':
 		text,image = gen_captcha_text_image()
 		print(image.shape)
 		print(text)
-		Image_height = 60
-		Image_width = 160
-		max_captcha = len(text)
+		IMAGE_HEIGHT = 60
+		IMAGE_WIDTH = 160
+		MAX_CAPTCHA = len(text)
 		char_set = number
-		char_set_len = len(number)
+		CHAR_SET_LEN = len(number)
 		X = tf.placeholder(tf.float32,[None, height*width])  # 计算有多少个像素点
-		Y = tf.placeholder(tf.float32,[None, max_captcha*char_set_len])  # 计算有多少位数字   4x10
+		Y = tf.placeholder(tf.float32,[None, MAX_CAPTCHA*CHAR_SET_LEN])  # 计算有多少位数字   4x10
 		keep_prob = tf.placeholder(tf.float32)
 		train_cnn()
 	if train ==1:
@@ -219,7 +228,7 @@ if __name__ == '__main__':
 		char_set = number
 		CHAR_SET_LEN = len(char_set)
 
-		text, image = gen_captcha_text_and_image()
+		text, image = gen_captcha_text_image()
 
 		f = plt.figure()
 		ax = f.add_subplot(111)
@@ -229,7 +238,7 @@ if __name__ == '__main__':
 		plt.show()
 
 		MAX_CAPTCHA = len(text)
-		image = convert2gray(image)
+		image = convert_gray(image)
 		image = image.flatten() / 255   #将像素转换为0-1之间
 
 		X = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT * IMAGE_WIDTH])# 计算有多少个像素点
